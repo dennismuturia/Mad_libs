4ae681ed07d484594a959e8b417e6166295b3a49
dennismuturia <dmuturia2922@gmail.com>
2017-08-14 10:01:18 +0300

    Added a for.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,3 +4,9 @@ import java.util.HashMap;
 import static spark.Spark.*;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
+
+public static void main(String []args){
+  String layout = "templates/layout.vtl";
+
+  
+}
